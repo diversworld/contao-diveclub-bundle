8f89ff51f07d455798a938b8d563709d9d4bbcea
Diversworld <eckhard@diversworld.eu>
2025-02-16 15:14:46 +0100

    Add language files for tl_dc_check_articles
    Introduce German and English translations for the tl_dc_check_articles module in the ContaoDiveclubBundle. These include legends, fields, and descriptions for managing TÜV appointments, vendor information, and invoice details.

---
+++ contao/languages/de/tl_dc_check_proposal.php
@@ -0,0 +1,51 @@
+<?php
+
+
+declare(strict_types=1);
+
+/*
+ * This file is part of ContaoDiveclubBundle.
+ *
+ * (c) Diversworld, Eckhard Becker 2025 <info@diversworld.eu>
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/diversworld/contao-diveclub-bundle
+ */
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['title_legend'] = "Rechnungsdaten";
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['details_legend'] = "Details zur Rechnung";
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['notes_legend'] = "Bemerkungen zur Rechnung";
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['publish_legend'] = "Veröffentlichen";
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['addCheckInfo'] = ["Daten zum TÜV-Termin erfassen", "Informationen zum TÜV-Termin erfassen"];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['addVendorInfo'] = ["Prüfungsunternehmen", "Informationen zum Prüfungsunternehmen erfassen."];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['addArticleInfo'] = ["Preisliste erstellen", "Preisliste für den TÜV-Termin erfassen"];
+
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['title'] = ["Angebotsnummer", "Geben Sie die Angebotsnummer ein."];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['alias'] = ["Alias", "Alias des Angebots. Wird automatisch erstellt."];
+
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['checkId']          = ['Termin','Event mit dem der TÜV-Termin im Kalender eingetragen wurde.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['proposalDate']     = ['Angebotsdatum','Datum des Angebots.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['vendorName']       = ['Prüfungsunternhemen','Name der Firma oder des Unternehmens.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['vendorWebsite']    = ['Website','Website der Firma oder des Unternehmens.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['vendorStreet']     = ['Straße','Straße der Firma oder des Unternehmens.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['vendorPostal']     = ['PLZ','Postleitzahl der Firma oder des Unternehmens.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['vendorCity']       = ['Ort','Ort der Firma oder des Unternehmens.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['vendorEmail']      = ['E-Mail','E-Mail der Firma oder des Unternehmens.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['vendorPhone']      = ['Telekon','Telefonnummer der Firma oder des Unternehmens.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['vendorMobile']     = ['Mobilnummer','Mobilnummer der Firma oder des Unternehmens.'];
+
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['priceTotal'] = ["Gesamtpreis", "Der gesamtpreis der rechnung wird automatisch berechnet."];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['notes'] = ["Bemerkungen", "Geben Sie eine Bemerkung ein"];
+
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['published'] = ["Veröffentlichen", "Den Datensatz veröffentlichen."];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['start'] = ["Anzeigen ab", "Ab wann soll der Datensatz angezeigt werden."];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['stop'] = ["Anzeigen bis", "Bis wann soll der Datensatz angezeigt werden."];
+
---
+++ contao/languages/en/tl_dc_check_proposal.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of ContaoDiveclubBundle.
+ *
+ * (c) Diversworld, Eckhard Becker 2025 <info@diversworld.eu>
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/diversworld/contao-diveclub-bundle
+ */
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['title_legend'] = "Invoice Data";
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['details_legend'] = "Invoice Details";
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['notes_legend'] = "Notes about the Invoice";
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['publish_legend'] = "Publish";
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['addCheckInfo'] = ["Record TÜV Appointment Data", "Enter information about the TÜV appointment."];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['addVendorInfo'] = ["Inspection Company", "Enter information about the inspection company."];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['addArticleInfo'] = ["Create Price List", "Record the price list for the TÜV appointment."];
+
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['title'] = ["Offer Number", "Enter the offer number."];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['alias'] = ["Alias", "Offer alias. Will be generated automatically."];
+
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['checkId']          = ['Appointment','Event in the calendar where the TÜV appointment was registered.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['proposalDate']     = ['Offer Date','The date of the offer.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['vendorName']       = ['Inspection Company','Name of the company or service provider.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['vendorWebsite']    = ['Website','Website of the company or service provider.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['vendorStreet']     = ['Street','Street address of the company or service provider.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['vendorPostal']     = ['Postal Code','Postal code of the company or service provider.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['vendorCity']       = ['City','City where the company or service provider is located.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['vendorEmail']      = ['Email','Email address of the company or service provider.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['vendorPhone']      = ['Phone','Phone number of the company or service provider.'];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['vendorMobile']     = ['Mobile Number','Mobile number of the company or service provider.'];
+
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['priceTotal'] = ["Total Price", "The total price of the invoice is calculated automatically."];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['notes'] = ["Notes", "Add a note."];
+
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['published'] = ["Publish", "Publish the record."];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['start'] = ["Show From", "Date from when the record should be visible."];
+$GLOBALS['TL_LANG']['tl_dc_check_articles']['stop'] = ["Show Until", "Date until when the record should be visible."];
