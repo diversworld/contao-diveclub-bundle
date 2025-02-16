ed5233d647de14556e40e2dbb63577141f0b60a9
Diversworld <eckhard@diversworld.eu>
2025-02-16 14:55:45 +0100

    Add custom label callback for regulator models
    Implemented a custom label callback to display human-readable model names for regulators based on the manufacturer. Adjusted language strings for improved clarity by adding primary/secondary indicators.

--- contao/dca/tl_dc_check_invoice.php
+++ contao/dca/tl_dc_check_invoice.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 /*
- * This file is part of DiveClub.
+ * This file is part of ContaoDiveclubBundle.
  *
- * (c) Diversworld 2024 <eckhard@diversworld.eu>
+ * (c) Diversworld, Eckhard Becker 2025 <info@diversworld.eu>
  * @license GPL-3.0-or-later
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
--- contao/dca/tl_dc_regulator_control.php
+++ contao/dca/tl_dc_regulator_control.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of ContaoDiveclubBundle.
  *
- * (c) DiversWorld 2024 <eckhard@diversworld.eu>
+ * (c) Diversworld, Eckhard Becker 2025 <info@diversworld.eu>
  * @license GPL-3.0-or-later
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
