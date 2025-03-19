41f1db349f8ca276ae05597933335929cd2278fe
Diversworld <eckhard@diversworld.eu>
2025-03-19 09:15:57 +0100

    Update language files and adjust course-related functionality
    Updated translations for course-related labels and fields in German and English language files. Fixed model method usage in DcListingController to ensure proper data handling and debugging support.

--- contao/languages/de/tl_dc_courses.php
+++ contao/languages/de/tl_dc_courses.php
@@ -16,11 +16,12 @@ declare(strict_types=1);
  * Legends
  */
 $GLOBALS['TL_LANG']['tl_dc_courses']['first_legend'] = "Basis Einstellungen";
-$GLOBALS['TL_LANG']['tl_dc_courses']['second_legend'] = "Erweiterte Einstellungen";
-
+$GLOBALS['TL_LANG']['tl_dc_courses']['details_section'] = "Kursdetails";
+$GLOBALS['TL_LANG']['tl_dc_courses']['image_legend'] = "Bild Einstellungen";
+$GLOBALS['TL_LANG']['tl_dc_courses']['publish_legend'] = "Veröffentlichen";
 /**
-* Global operations
-*/
+ * Global operations
+ */
 $GLOBALS['TL_LANG']['tl_dc_courses']['new'] = ["Neu", "Ein neues Element anlegen"];
 
 /**
@@ -34,12 +35,19 @@ $GLOBALS['TL_LANG']['tl_dc_courses']['show'] = "Datensatz mit ID: %s ansehen";
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_dc_courses']['title'] = ["Titel", "Geben Sie den Titel ein"];
-$GLOBALS['TL_LANG']['tl_dc_courses']['selectField'] = ["Select Feld", "Wählen Sie aus."];
-$GLOBALS['TL_LANG']['tl_dc_courses']['checkboxField'] = ["Chosen Feld", "Wählen Sie aus."];
-$GLOBALS['TL_LANG']['tl_dc_courses']['multitextField'] = ["Multitext Feld", "Geben Sie die Werte ein"];
-$GLOBALS['TL_LANG']['tl_dc_courses']['addSubpalette'] = ["Erweiterte Einstellungen aktivieren", "Hier können Sie die erweiterten Einstellungen aktivieren."];
-$GLOBALS['TL_LANG']['tl_dc_courses']['textareaField'] = ["Textarea", "Geben Sie einen Text ein"];
+$GLOBALS['TL_LANG']['tl_dc_courses']['title'] 			= ["Titel", "Geben Sie den Titel ein"];
+$GLOBALS['TL_LANG']['tl_dc_courses']['alias'] 			= ["Alias", "Geben Sie den Alias ein"];
+$GLOBALS['TL_LANG']['tl_dc_courses']['category'] 		= ["Kategorie", "Wählen Sie die Kurskategorie aus."];
+$GLOBALS['TL_LANG']['tl_dc_courses']['description'] 	= ["Beschreibung", "Bitte geben Sie die Kursbeschreibung an."];
+$GLOBALS['TL_LANG']['tl_dc_courses']['requirements'] 	= ["Voraussetzungen", "Geben Sie die Voraussetzngen für die Kursteilnahme ein."];
+$GLOBALS['TL_LANG']['tl_dc_courses']['addImage'] 		= ["Bild hinzufügen", "Hier können Sie die Bild Einstellungen aktivieren."];
+$GLOBALS['TL_LANG']['tl_dc_courses']['fullsize'] 		= ["Textarea", "Geben Sie einen Text ein"];
+$GLOBALS['TL_LANG']['tl_dc_courses']['size'] 			= ["Bildgröße", "Geben Sie die Bildgröße an. "];
+$GLOBALS['TL_LANG']['tl_dc_courses']['floating'] 		= ["Anordnung", "Wo soll das Bild angezeigt werden. "];
+$GLOBALS['TL_LANG']['tl_dc_courses']['overwriteMeta'] 	= ["Metadaten überschreiben", "Überschreiben Sie die Metadaten des Bildes."];
+$GLOBALS['TL_LANG']['tl_dc_courses']['published']	= ['Veröffentlicht', 'Markieren Sie das Equipment als veröffentlicht.'];
+$GLOBALS['TL_LANG']['tl_dc_courses']['start']        = ['Startdatum', 'Geben Sie ein Startdatum an.'];
+$GLOBALS['TL_LANG']['tl_dc_courses']['stop']         = ['Enddatum', 'Geben Sie ein Enddatum an.'];
 
 /**
  * References
--- contao/languages/en/tl_dc_courses.php
+++ contao/languages/en/tl_dc_courses.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of diveclub.
+ * This file is part of ContaoDiveclubBundle.
  *
  * (c) Diversworld 2024 <eckhard@diversworld.eu>
  * @license GPL-3.0-or-later
@@ -11,42 +11,51 @@ declare(strict_types=1);
  * please view the LICENSE file that was distributed with this source code.
  * @link https://github.com/diversworld/contao-diveclub-bundle
  */
+
 /**
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_dc_courses']['first_legend'] = "Basic Settings";
-$GLOBALS['TL_LANG']['tl_dc_courses']['second_legend'] = "Advanced Settings";
-
+$GLOBALS['TL_LANG']['tl_dc_courses']['first_legend'] = "Basis Einstellungen";
+$GLOBALS['TL_LANG']['tl_dc_courses']['details_section'] = "Kursdetails";
+$GLOBALS['TL_LANG']['tl_dc_courses']['image_legend'] = "Bild Einstellungen";
+$GLOBALS['TL_LANG']['tl_dc_courses']['publish_legend'] = "Veröffentlichen";
 /**
  * Global operations
  */
-$GLOBALS['TL_LANG']['tl_dc_courses']['new'] = ["New", "Create a new element"];
+$GLOBALS['TL_LANG']['tl_dc_courses']['new'] = ["Neu", "Ein neues Element anlegen"];
 
 /**
  * Operations
  */
-$GLOBALS['TL_LANG']['tl_dc_courses']['edit'] = "Edit record with ID: %s";
-$GLOBALS['TL_LANG']['tl_dc_courses']['copy'] = "Copy record with ID: %s";
-$GLOBALS['TL_LANG']['tl_dc_courses']['delete'] = "Delete record with ID: %s";
-$GLOBALS['TL_LANG']['tl_dc_courses']['show'] = "View record with ID: %s";
+$GLOBALS['TL_LANG']['tl_dc_courses']['edit'] = "Datensatz mit ID: %s bearbeiten";
+$GLOBALS['TL_LANG']['tl_dc_courses']['copy'] = "Datensatz mit ID: %s kopieren";
+$GLOBALS['TL_LANG']['tl_dc_courses']['delete'] = "Datensatz mit ID: %s löschen";
+$GLOBALS['TL_LANG']['tl_dc_courses']['show'] = "Datensatz mit ID: %s ansehen";
 
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_dc_courses']['title'] = ["Title", "Enter the title"];
-$GLOBALS['TL_LANG']['tl_dc_courses']['selectField'] = ["Select Field", "Please select."];
-$GLOBALS['TL_LANG']['tl_dc_courses']['checkboxField'] = ["Chosen Field", "Please choose."];
-$GLOBALS['TL_LANG']['tl_dc_courses']['multitextField'] = ["Multitext Field", "Enter the values"];
-$GLOBALS['TL_LANG']['tl_dc_courses']['addSubpalette'] = ["Enable Advanced Settings", "Here you can enable the advanced settings."];
-$GLOBALS['TL_LANG']['tl_dc_courses']['textareaField'] = ["Textarea", "Enter a text"];
+$GLOBALS['TL_LANG']['tl_dc_courses']['title'] 			= ["Titel", "Geben Sie den Titel ein"];
+$GLOBALS['TL_LANG']['tl_dc_courses']['alias'] 			= ["Alias", "Geben Sie den Alias ein"];
+$GLOBALS['TL_LANG']['tl_dc_courses']['category'] 		= ["Kategorie", "Wählen Sie die Kurskategorie aus."];
+$GLOBALS['TL_LANG']['tl_dc_courses']['description'] 	= ["Beschreibung", "Bitte geben Sie die Kursbeschreibung an."];
+$GLOBALS['TL_LANG']['tl_dc_courses']['requirements'] 	= ["Voraussetzungen", "Geben Sie die Voraussetzngen für die Kursteilnahme ein."];
+$GLOBALS['TL_LANG']['tl_dc_courses']['addImage'] 		= ["Bild hinzufügen", "Hier können Sie die Bild Einstellungen aktivieren."];
+$GLOBALS['TL_LANG']['tl_dc_courses']['fullsize'] 		= ["Textarea", "Geben Sie einen Text ein"];
+$GLOBALS['TL_LANG']['tl_dc_courses']['size'] 			= ["Bildgröße", "Geben Sie die Bildgröße an. "];
+$GLOBALS['TL_LANG']['tl_dc_courses']['floating'] 		= ["Anordnung", "Wo soll das Bild angezeigt werden. "];
+$GLOBALS['TL_LANG']['tl_dc_courses']['overwriteMeta'] 	= ["Metadaten überschreiben", "Überschreiben Sie die Metadaten des Bildes."];
+$GLOBALS['TL_LANG']['tl_dc_courses']['published']	= ['Veröffentlicht', 'Markieren Sie das Equipment als veröffentlicht.'];
+$GLOBALS['TL_LANG']['tl_dc_courses']['start']        = ['Startdatum', 'Geben Sie ein Startdatum an.'];
+$GLOBALS['TL_LANG']['tl_dc_courses']['stop']         = ['Enddatum', 'Geben Sie ein Enddatum an.'];
 
 /**
  * References
  */
-$GLOBALS['TL_LANG']['tl_dc_courses']['firstoption'] = "First Option";
-$GLOBALS['TL_LANG']['tl_dc_courses']['secondoption'] = "Second Option";
+$GLOBALS['TL_LANG']['tl_dc_courses']['firstoption'] = "Erste Option";
+$GLOBALS['TL_LANG']['tl_dc_courses']['secondoption'] = "Zweite Option";
 
 /**
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_dc_courses']['customButton'] = "Start Custom Routine";
+$GLOBALS['TL_LANG']['tl_dc_courses']['customButton'] = "Custom Routine starten";
